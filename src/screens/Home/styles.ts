944dbd9270c050a49b7fa798458af8819ee28ec2
victorb132 <victor96novais@gmail.com>
2021-06-03 10:37:22 -0300

    Header

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,4 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -10,4 +11,19 @@ export const Header = styled.View`
   height: 113px;
 
   background-color: ${({ theme }) => theme.colors.header};
+  
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
