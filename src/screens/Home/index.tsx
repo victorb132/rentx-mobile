5a5b01e61ddc1067c442b521647f5d20c2c50cf9
victorb132 <victor96novais@gmail.com>
2021-06-03 10:17:16 -0300

    Initial Screen and style global

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import {
+ Container,
+ Header
+} from './styles';
+
+export function Home(){
+ return (
+   <Container>
+     <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+     />
+     <Header>
+       
+     </Header>
+   </Container>
+ );
+}
